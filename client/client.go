b9f170130ce6c9106909a92c17a47e89887e6f97
Erik Lima <elima@visaogeo.com.br>
2023-01-27 22:01:43 -0300

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"runtime"
+	"sync"
+
+	"github.com/lucas-clemente/quic-go/http3"
+)
+
+var certPath string
+var loopCount int = 10
+
+func init() {
+	setupCertPath()
+}
+
+func setupCertPath() {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("Failed to get current frame")
+	}
+
+	certPath = path.Dir(filename)
+}
+
+func main() {
+	pool := getCertPool()
+	addRootCA(pool)
+
+	client := createClient(pool)
+	url := "https://localhost:3001"
+	var wg sync.WaitGroup
+
+	wg.Add(loopCount)
+	for loopCount > 0 {
+		go executeClient(client, url, &wg)
+		loopCount--
+	}
+	wg.Wait()
+
+	fmt.Println("Client finished.")
+}
+
+func getCertPool() *x509.CertPool {
+	pool, err := x509.SystemCertPool()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pool
+}
+
+func addRootCA(certPool *x509.CertPool) {
+	caCertPath := path.Join(certPath, "ca.pem")
+	caCertRaw, err := os.ReadFile(caCertPath)
+	if err != nil {
+		panic(err)
+	}
+	if ok := certPool.AppendCertsFromPEM(caCertRaw); !ok {
+		panic("FAILURE: Could not add root ceritificate to pool.")
+	}
+}
+
+func createClient(pool *x509.CertPool) *http.Client {
+	tlsConfig := &tls.Config{
+		RootCAs:            pool,
+		InsecureSkipVerify: false,
+		// KeyLogWriter: ,
+	}
+
+	roundTripper := &http3.RoundTripper{
+		TLSClientConfig: tlsConfig,
+		QuicConfig:      nil,
+	}
+
+	defer roundTripper.Close()
+
+	hclient := &http.Client{
+		Transport: roundTripper,
+	}
+
+	return hclient
+}
+
+func executeClient(client *http.Client, url string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	response, err := client.Get(url)
+
+	if err != nil {
+		log.Fatal("Request error: ", err)
+	}
+
+	fmt.Printf("Resposta para %s: %#v\n", url, response)
+
+	body := getBody(response)
+	fmt.Printf("Body: %s\n", body)
+}
+
+func getBody(response *http.Response) []byte {
+	body := &bytes.Buffer{}
+
+	_, err := io.Copy(body, response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return body.Bytes()
+}
---
+++ server.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"path"
+	"runtime"
+	"sync"
+
+	"github.com/lucas-clemente/quic-go/http3"
+)
+
+var certPath string
+
+func init() {
+	setupCertPath()
+}
+
+func setupCertPath() {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		panic("Failed to get current frame")
+	}
+
+	certPath = path.Dir(filename)
+}
+
+func main() {
+	addr := "localhost:3001"
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go startServer(addr, &wg)
+	wg.Wait()
+
+	fmt.Println("Server finished")
+}
+
+func startServer(addr string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	handler := setupHandler()
+
+	server := http3.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	pem, key := getCertificatePaths()
+	fmt.Println(pem, key)
+
+	err := server.ListenAndServeTLS(pem, key)
+
+	if err != nil {
+		fmt.Printf("Server error: %s\n", err)
+	} else {
+		fmt.Println("Server OK")
+	}
+}
+
+func setupHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("Request recebido: %s\n", r.RequestURI)
+		// w.Write([]byte(r.RequestURI))
+		w.Write([]byte(r.URL.String()))
+	})
+
+	return mux
+}
+
+func getCertificatePaths() (string, string) {
+	return path.Join(certPath, "cert.pem"), path.Join(certPath, "priv.key")
+}
