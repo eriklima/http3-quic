b3af092d292d140a30e6bb1a3cebd6e49b84ffbf
Erik Lima <elima@visaogeo.com.br>
2023-03-07 22:19:17 -0300

    build: receber parâmetros externos ao subir o docker

--- simulator/run_endpoint.sh
+++ simulator/run_endpoint.sh
@@ -15,12 +15,16 @@ if [ "$ROLE" == "client" ]; then
     echo "Wait for the simulator to start up."
     /wait-for-it.sh sim:57832 -s -t 30
 
-    echo "Run the client HTTP3 requesting to 193.167.100.100:4433"
-    go run client/client.go -url 193.167.100.100:4433
+    echo "Request server HTTP3 to 193.167.100.100:4433"
+    echo "Client params: $CLIENT_PARAMS"
+
+    cd client && ./client -url 193.167.100.100:4433 $CLIENT_PARAMS
 elif [ "$ROLE" == "server" ]; then
     # It is recommended to increase the maximum buffer size (https://github.com/quic-go/quic-go/wiki/UDP-Receive-Buffer-Size)
     # sysctl -w net.core.rmem_max=2500000
 
     echo "Run the server HTTP3 on 0.0.0.0:4433"
-    go run server.go -addr 0.0.0.0:4433
+    echo "Server params: $SERVER_PARAMS"
+
+    ./server -addr 0.0.0.0:4433 $SERVER_PARAMS
 fi
